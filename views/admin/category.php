28e15325bf60338ea739863c1016f37b38cfe05f
jaber-git <post2jaber@gmail.com>
2020-04-28 14:33:55 +0600

    category list table design and edit delete option tut34full

--- views/admin/category.php
+++ views/admin/category.php
@@ -1,8 +1,28 @@
 
 <h2>Category List</h2>
-<?php
+
+<table class="tblone">
+    <tr>
+        <th>Serial no</th>
+        <th>Category Name</th>
+        <th>Category Title</th>
+        <th> Action</th>
+    </tr>
+    <tr>
+    <?php
+$i=0; 
 foreach($cat as $key => $value){
-    echo $value['name']."<br/>";
-  
-}
+  $i++; 
 ?>
+        <td><?php echo $i;?></td>
+        <td><?php echo $value['name'];?> </td>
+        <td><?php echo $value['title'];?></td>
+        <td>
+            <a href="<?php echo BASE_URL;?>/Admin/editCat">Edit</a>
+           <a href="<?php echo BASE_URL;?>/Admin/deleteCat"">Delete</a>
+        </td>
+
+
+    </tr>
+<?php } ?>
+</table>
